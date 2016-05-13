ed14f784df3331854e6458f5a0cc6f529a70414b
carlosthe19916 <carlosthe19916@gmail.com>
2016-05-13 12:41:28 -0500

    pre beta version

--- src/ng-qzTray.js
+++ src/ng-qzTray.js
@@ -1,28 +1,27 @@
 (function () {
     'use strict';
 
-    window.qz.api.setPromiseType(Promise.fromCallback);
-
     var module = angular.module('ng-qzTray', ['LocalStorageModule'])
         .constant('QzTray', window.qz)
-        .constant('QzTrayStorage', {
+        .constant('QzTrayConnectConfig', {
+            host: 'localhost',
+            usingSecure: true,
+            keepAlive: 60,
+            retries: 5,
+            delay: 1
+        })
+        .constant('QzTrayPrinterConfig', {
             printer: {
                 name: undefined
             },
-            options: {
-                host: 'localhost',
-                usingSecure: true,
-                retries: 5,
-                delay: 1
-            },
-            config: {
-
-            }
+            config: {}
         })
         .provider('QzTrayConfig', function () {
             var _autoConnect = false;
+
             var _localStorage = false;
-            var _localStorageName = 'QzTrayConfigLocalStorage';
+            var _connectConfigLocalStorageName = 'opensales_connectConfigLocalStorageName';
+            var _printerConfigLocalStorageName = 'opensales_printerConfigLocalStorageName';
 
             return {
                 setAutoConnect: function (value) {
@@ -31,20 +30,24 @@
                 setLocalStorage: function (value) {
                     _localStorage = value;
                 },
-                setLocalStorageName: function (value) {
-                    _localStorageName = value;
+                setConnectConfigLocalStorageName: function (value) {
+                    _connectConfigLocalStorageName = value;
+                },
+                setPrinterConfigLocalStorageName: function (value) {
+                    _printerConfigLocalStorageName = value;
                 },
 
                 $get: function () {
                     return {
                         autoConnect: _autoConnect,
                         localStorage: _localStorage,
-                        localStorageName: _localStorageName
+                        connectConfigLocalStorageName: _connectConfigLocalStorageName,
+                        printerConfigLocalStorageName: _printerConfigLocalStorageName
                     };
                 }
             };
         })
-        .factory('QzTrayService', ['QzTray', 'QzTrayConfig', 'localStorageService', function (QzTray, QzTrayConfig, localStorageService) {
+        .factory('QzTrayService', ['$rootScope', 'QzTray', 'QzTrayConnectConfig', 'QzTrayPrinterConfig', 'QzTrayConfig', 'localStorageService', function ($rootScope, QzTray, QzTrayConnectConfig, QzTrayPrinterConfig, QzTrayConfig, localStorageService) {
 
             var service = {};
             service.cfg = null;
@@ -57,11 +60,26 @@
                     throw new Error('An active connection with QZ already exists.');
                 }
             };
-
-            service.save = function () {
-                localStorageService.set(QzTrayConfig.localStorageName, JSON.stringify(service.cfg));
+            service.endConnection = function () {
+                if (QzTray.websocket.isActive()) {
+                    return QzTray.websocket.disconnect();
+                } else {
+                    throw new Error('No active connection with QZ exists.');
+                }
+            };
+            service.isActive = function () {
+                return QzTray.websocket.isActive();
             };
 
+            /// Storage ///
+            service.saveConnectConfig = function () {
+                localStorageService.set(QzTrayConfig.connectConfigLocalStorageName, JSON.stringify(QzTrayConnectConfig));
+            };
+            service.savePrinterConfig = function () {
+                localStorageService.set(QzTrayConfig.printerConfigLocalStorageName, JSON.stringify(QzTrayPrinterConfig));
+            };
+
+            /// Update config ///
             service.getUpdatedConfig = function () {
                 if (service.cfg == null) {
                     service.cfg = QzTray.configs.create(null);
@@ -71,6 +89,9 @@
             };
             service.updateConfig = function (config) {
                 service.cfg.reconfigure(config);
+                if(!$rootScope.$$phase) {
+                    $rootScope.$apply();
+                }
             };
 
 
@@ -86,6 +107,9 @@
             };
 
             /// Printers ///
+            service.getConfig = function () {
+                return service.cfg.config;
+            };
             service.getPrinter = function () {
                 return service.cfg.getPrinter();
             };
@@ -93,7 +117,7 @@
                 return service.cfg.getOptions();
             };
 
-            service.print = function (printData, config) {
+            service.print = function (printData) {
                 var config = service.getUpdatedConfig();
                 return QzTray.print(config, printData);
             };
@@ -101,6 +125,9 @@
             service.setPrinter = function (printer) {
                 var cf = service.getUpdatedConfig();
                 cf.setPrinter(printer);
+                if(!$rootScope.$$phase) {
+                    $rootScope.$apply();
+                }
             };
 
             return service;
@@ -108,7 +135,7 @@
         .factory('QzTrayPrinter', function () {
 
         })
-        .run(function (QzTrayConfig, QzTrayService, QzTrayStorage, localStorageService) {
+        .run(function (QzTrayConfig, QzTrayService, QzTrayConnectConfig, QzTrayPrinterConfig, localStorageService) {
 
             var showError = function (err) {
                 alert(err);
@@ -116,15 +143,16 @@
 
             /// If localStorage is enabled, load config from localstorage
             if (QzTrayConfig.localStorage) {
-                angular.extend(QzTrayStorage, angular.fromJson(localStorageService.get(QzTrayConfig.localStorageName)));
+                angular.extend(QzTrayConnectConfig, angular.fromJson(localStorageService.get(QzTrayConfig.connectConfigLocalStorageName)));
+                angular.extend(QzTrayPrinterConfig, angular.fromJson(localStorageService.get(QzTrayConfig.printerConfigLocalStorageName)));
             }
 
             /// If autoConnect is enabled
             if (QzTrayConfig.autoConnect) {
-                QzTrayService.startConnection(QzTrayStorage.options).then(function () {
-                    if (QzTrayStorage.printer.name) {
-                        QzTrayService.findPrinter(QzTrayStorage.printer.name).then(function (data) {
-                            QzTrayService.setPrinter(QzTrayStorage.printer);
+                QzTrayService.startConnection(QzTrayConnectConfig).then(function () {
+                    if (QzTrayPrinterConfig.printer.name) {
+                        QzTrayService.findPrinter(QzTrayPrinterConfig.printer.name).then(function (data) {
+                            QzTrayService.setPrinter(QzTrayPrinterConfig.printer);
                         }).catch(showError);
                     } else {
                         QzTrayService.findDefaultPrinter().then(function (data) {
